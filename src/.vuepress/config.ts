89f413558f30313ceeb16f8914507a444a385499
zhf521 <3407085928@qq.com>
2024-02-07 16:29:00 +0800

    设置base路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,15 @@
-import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { defineUserConfig } from 'vuepress';
+import theme from './theme.js';
 
 export default defineUserConfig({
-  base: "/",
+    base: '/MyBlog/',
 
-  lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+    lang: 'zh-CN',
+    title: '博客演示',
+    description: 'vuepress-theme-hope 的博客演示',
 
-  theme,
+    theme,
 
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
+    // 和 PWA 一起启用
+    // shouldPrefetch: false,
 });
